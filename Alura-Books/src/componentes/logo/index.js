b063088ea94a5dfdf41cf4270526563d4724a2e4
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-07-01 18:12:47 -0300

    Update

--- Alura-Books/src/componentes/logo/index.js
+++ Alura-Books/src/componentes/logo/index.js
@@ -1,11 +1,25 @@
-import './style.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
 
-function Logo(){
-    return(
-        <div className="logo">
-          <img src={logo} alt="logo"></img>
-          <p><strong>Alura</strong> Books</p>
-        </div>
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
+
+function Logo() {
+    return (
+        <LogoContainer>
+            <LogoImage
+                src={logo}
+                alt='logo' 
+            />
+            <p><strong>Alura</strong>Books</p>
+       </LogoContainer>
     )
 }
+
+export default Logo
